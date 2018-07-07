6a1d77cadc8e2f0841c42e20e7882a28dfafee81
Aon1840 <aonattapon1840.ap@gmail.com>
2018-07-07 14:17:19 +0700

    Add simple method for module building

--- Buildings/views.py
+++ Buildings/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from Buildings.models import Building
+from Emulators.serializers import BuildingSerializer
+import json
 
 # Create your views here.
+@csrf_exempt
+def getAllBuilding(request):
+    if request.method == 'GET':
+        buildings = Building.objects.all()
+        serializer = BuildingSerializer(buildings, many=True)
+        return JsonResponse(serializer, safe=False)
+
+#     -----------------------------  TEST Method  -------------------------------------------
+@csrf_exempt
+def testAddBuilding(request):
+    serialzer = BuildingSerializer(data=request.POST)
+    if serialzer.is_valid():
+        serialzer.save()
+        return JsonResponse(serialzer.data, status=201)
+
+    return JsonResponse(serialzer.errors, status=400)
+
+# @csrf_exempt
+# def testEditBuilding(request):
